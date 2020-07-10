7fabd07746c4923ef4edc3b32ffee6543f83ee52
Santi Dasari <dsantisree@gmail.com>
2020-07-10 18:14:27 -0500

    First Commit

---
+++ QuickPick/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  QuickPick
+//
+//  Created by Santi Dasari on 7/10/20.
+//  Copyright © 2020 Santi Dasari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+         
+    //setting some characteristics for the start button like rounded edges
+        startButton.layer.cornerRadius = 25.0
+    }
+    
+   
+    
+    
+
+   
+    
+}
+
